c2079aa3b2f479c7de7821b0c3c7a3bff7d0b9a9
Laurent Gautier <laurent.gautier@novartis.com>
2015-06-19 12:36:58 -0400

    earlier fix had syntax error

--- src/test/test_core.py
+++ src/test/test_core.py
@@ -15,7 +15,8 @@
 import unittest
 import tempfile, shutil, gzip
 
-from railroadtracks import core, model.simulate
+from railroadtracks import core
+import railroadtracks.model.simulate
 
 try:
     import ngs_plumbing
@@ -96,7 +97,7 @@ class AssetsTestCase(unittest.TestCase):
         read1_fh.close()
         read2_fh = tempfile.NamedTemporaryFile(prefix='read2', suffix='.fq.gz', dir=self.tempdir, delete=False)
         read2_fh.close()
-        with open(model.simulate.PHAGEFASTA) as fasta_fh:
+        with open(railroadtracks.model.simulate.PHAGEFASTA) as fasta_fh:
             reference = next(railroadtracks.model.simulate.readfasta_iter(fasta_fh))
             read1_io = gzip.GzipFile(read1_fh.name, mode='w')
             read2_io = gzip.GzipFile(read2_fh.name, mode='w')
